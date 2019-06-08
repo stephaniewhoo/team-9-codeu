af41f7a2526bb6b458cc978f3df306337d662167
Stephanie <stephaniewhoo@hotmail.com>
2019-06-08 17:54:56 -0400

    week3Push1 allow basic html

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -76,8 +76,8 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
-    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
-
+    String text = Jsoup.clean(request.getParameter("text"), Whitelist.basic());
+    //change to allow HTML tags
     Message message = new Message(user, text);
     datastore.storeMessage(message);
 
